60716bc2225089618e2f80eb7e2cd9acc386fcaa
SecretFr <linkclean@naver.com>
2022-07-27 15:58:58 +0900

    variable & const

---
+++ src/section2/const1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//상수
+	//const 사용 초기화, 한 번 선언 후 값 변경 금징, 고정 된 값 관리 용
+	const a string = "Test1"
+	const b = "Test2"
+	const c int32 = 10 * 10
+
+	//함수를 사용해서 리턴을 받으면 에러 발생
+	//const d = getHeight()
+
+	const e = 35.6
+	const f = false
+	/*
+	   에러 발생
+	   const g string
+	   g = "Test3"
+	*/
+
+	fmt.Println("a : ", a, "b : ", b, "c : ", c, "e : ", e, "f : ", f)
+}
